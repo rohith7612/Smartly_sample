c850fab900dbde5bb80912bcaa1064ce8138761d
rohith7612 <prudhvirohith45@gmail.com>
2025-10-29 23:13:08 +0530

    Added postgresql database.

--- smartly/settings.py
+++ smartly/settings.py
@@ -86,16 +86,10 @@ WSGI_APPLICATION = 'smartly.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
+import dj_database_url
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'smartly',
-            'USER': 'root',
-            'PASSWORD': 'root',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-        }
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 
 # Password validation
